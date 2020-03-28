dba9fda6916d6fc0ff9d3c14a505c86bc092fe27
Tcaprioli <Capriolityler@gmail.com>
2020-03-28 15:20:25 -0400

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -11,3 +11,19 @@ let state = {count: 0}
 let action = {type: 'INCREASE_COUNT'}
 
 changeState(state, action)
+function render(){
+  document.body.textContent = state.count
+}
+
+function dispatch(action){
+  state = changeState(state, action)
+  render()
+  return state
+}
+
+dispatch({type: 'INCREASE_COUNT'})
+dispatch({type: 'INCREASE_COUNT'})
+dispatch({type: 'INCREASE_COUNT'})
+dispatch({type: 'INCREASE_COUNT'})
+
+
